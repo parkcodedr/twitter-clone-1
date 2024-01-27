48582f7827333b7faea3f6a38661009720233a2e
ParkCodeDr <okwori32@gmail.com>
2024-01-27 13:53:35 +0100

    clone

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,6 +2,8 @@ import type { AppProps } from 'next/app'
 import { Toaster } from 'react-hot-toast';
 import { SessionProvider } from 'next-auth/react';
 
+//twitter clone
+
 import Layout from '@/components/Layout'
 import LoginModal from '@/components/modals/LoginModal'
 import RegisterModal from '@/components/modals/RegisterModal'
